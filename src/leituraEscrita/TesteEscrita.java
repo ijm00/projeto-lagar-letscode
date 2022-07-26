b114d4332bf8cfc5ff9c2ccbb893f53ead36cb9e
F8934921 Salvio Medeiros Costa Neto <salvio.neto@bb.com.br>
2022-07-26 17:29:15 -0300

    WIP_readWrite

--- src/leituraEscrita/TesteEscrita.java
+++ src/leituraEscrita/TesteEscrita.java
@@ -1,3 +1,4 @@
+package leituraEscrita;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
--- src/leituraEscrita/TesteLeitura.java
+++ src/leituraEscrita/TesteLeitura.java
@@ -1,3 +1,4 @@
+package leituraEscrita;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
