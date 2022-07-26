31072390425d9c90bda3009297d601776bd94936
Rachel <rachel.botelho@bb.com.br>
2022-07-26 17:29:27 -0300

    método para extrair numros de string

--- src/App.java
+++ src/App.java
@@ -1,6 +1,25 @@
+import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
+        List<String> numeros = new ArrayList<>();
+        
+        Pattern p = Pattern.compile("(\\d+).(\\d+)");
+        Matcher m = p.matcher("string 123.489more567string890");
+        while(m.find()) {
+            numeros.add(m.group());
+        }
+
+        for (String numero : numeros){
+            System.out.println(numero);
+        }
+
     }
+
+    //(\\w*)\\d+(\\w*)
+    // \t - tab
 }
